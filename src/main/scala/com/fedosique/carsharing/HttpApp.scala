0c0fdf396dcabb630a50aa6c55117b5bddf9be70
fedix <d.fedor97@gmail.com>
2020-11-26 16:35:42 +0300

    Moved storage initialization to HttpApp

--- src/main/scala/com/fedosique/carsharing/HttpApp.scala
+++ src/main/scala/com/fedosique/carsharing/HttpApp.scala
@@ -7,13 +7,21 @@ import com.fedosique.carsharing.api.ClientServiceRoutes
 import com.fedosique.carsharing.logic.ClientServiceImpl
 import com.fedosique.carsharing.storage.InMemoryCarStorage
 
-
 object HttpApp extends App {
 
   implicit val actorSystem: ActorSystem = ActorSystem()
   implicit val ec = actorSystem.dispatcher
 
   val storage = new InMemoryCarStorage
+  val kia = Car("kia rio", "blue", "а117рп78", 60.787842, 55.848593, 1, isOccupied = false)
+  val bmw = Car("bmw 3", "black", "г651та78", 60.787842, 70.342421, 0.5, isOccupied = true)
+
+  val initDB = for {
+    _ <- storage.put(1, kia)
+    _ <- storage.put(2, bmw)
+  } yield ()
+  initDB.runToFuture(monix.execution.Scheduler.Implicits.global)
+
   val service = new ClientServiceImpl(storage)
   val apiRoutes = pathPrefix("api" / "v1") {
     new ClientServiceRoutes(service).routes
--- src/main/scala/com/fedosique/carsharing/storage/InMemoryCarStorage.scala
+++ src/main/scala/com/fedosique/carsharing/storage/InMemoryCarStorage.scala
@@ -16,10 +16,5 @@ class InMemoryCarStorage extends CarStorage[Task] {
 
   override def contains(id: Int): Task[Boolean] = Task(storage.contains(id))
 
-
   private val storage = new TrieMap[Int, Car]
-
-  val kia = Car("kia rio", "blue", "а117рп78", 60.787842, 55.848593, 1, isOccupied = false)
-  val bmw = Car("bmw 3", "black", "г651та78", 60.787842, 70.342421, 0.5, isOccupied = true)
-  storage ++= Seq(1 -> kia, 2 -> bmw)
 }
